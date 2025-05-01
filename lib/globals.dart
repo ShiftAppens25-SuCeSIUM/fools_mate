e9e89cfa26f5f19b3232a61d584fe0f1e96d0b0e
VitorLelis <vitorll909@gmail.com>
2025-05-01 20:48:39 +0100

    feat(home): home page card layout

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
-    static const Color primary = Color(0xFF8A5CF6);
+class AppColors {
+  static const Color primary = Color(0xFF8A5CF6);
+  static const Color card = Colors.white;
+  static const Color background = Color.fromRGBO(245, 245, 245, 1);
+  static const Color slogan = Color.fromRGBO(97, 97, 97, 1);
 }
